43ae7f785921761a416cc7436d8f95d0005f0db2
Hycric <96342569+hycric@users.noreply.github.com>
2023-07-14 12:17:20 -0400

    Add files via upload

---
+++ Assignment2/quadratic_solver.py
@@ -0,0 +1,27 @@
+"""
+File: quadratic_solver.py
+Name:
+-----------------------
+This program should implement a console program
+that asks 3 inputs (a, b, and c)
+from users to compute the roots of equation:
+ax^2 + bx + c = 0
+Output format should match what is shown in the sample
+run in the Assignment 2 Handout.
+
+"""
+
+import math
+
+
+def main():
+	"""
+	TODO:
+	"""
+	pass
+
+
+###### DO NOT EDIT CODE BELOW THIS LINE ######
+
+if __name__ == "__main__":
+	main()
